4ccc5a91390722f7e6b6f7d3c8f0b383352073e9
Cathy Descoutures <cathy.descoutures@etu.unilim.fr>
2025-02-04 11:13:33 +0100

    ajout champ formulaire

--- app/src/main/java/com/example/r4a11_tp3/MainActivity.kt
+++ app/src/main/java/com/example/r4a11_tp3/MainActivity.kt
@@ -8,13 +8,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -73,6 +79,7 @@ fun HomeScreen(navController: NavController){
 
 @Composable
 fun FormScreen(navController: NavController){
+    var name by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -87,4 +94,12 @@ fun FormScreen(navController: NavController){
             Text(text = "Retour")
         }
     }
+    TextField(
+        value = name,
+        onValueChange = { newText -> name = newText },
+        label = { Text("Entrez votre nom") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    )
 }
